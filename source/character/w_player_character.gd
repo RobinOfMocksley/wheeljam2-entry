9903eae513883a3f62f574e7c20e204772e34ea4
Thomas <thomashapplewhite@gmail.com>
2026-05-04 21:19:41 -0700

    Dodging now follows the direction the player is facing

--- source/character/w_player_character.gd
+++ source/character/w_player_character.gd
@@ -22,6 +22,7 @@ class_name WPlayerCharacter
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	setup()
+	_abilities.append(dodge_ability)
 	#emerged.connect(_on_controller_emerged.bind())
 	#submerged.connect(_on_controller_subemerged.bind())
 
@@ -29,49 +30,14 @@ func _ready():
 func _physics_process(delta):
 	var is_valid_input := Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED
 	
-	# First, rotate towards whatever we are Z-targeting, if anything.
-	#rotate_head(somehow)
-	
-	#Second, rotate our current velocity to go with it
-	#this might be done for me
-	#velocity = velocity
-	
-	#Third, handle the dodge!
 	if is_valid_input:
-		var dodge_dir : Vector3 = _determine_dodge_direction()
-		velocity = dodge_ability.apply(velocity, 0.0, is_on_floor(), dodge_dir, delta)
-		
-	move(delta)
+		var input_axis = Input.get_vector(input_left_action_name, input_right_action_name, input_back_action_name, input_forward_action_name)
+		move(delta, input_axis, false, false, false, false, false)
+	else:
+		move(delta)
 
 
 func _input(event: InputEvent) -> void:
 	# Mouse look (only if the mouse is captured).
 	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		rotate_head(event.screen_relative)
-
-func _determine_dodge_direction() -> Vector3:
-	# The direction the camera is looking
-	#var dir : Vector3 = camera_ref.transform.basis.z
-	var dir : Vector3 
-	# Flat plane direction of camera facing
-	#dir.y = 0.0
-	#dir = dir.normalized()
-	
-	if Input.is_action_just_pressed("move_forward"):
-		dir.z -= 1.0
-	if Input.is_action_just_pressed("move_backward"):
-		dir.z += 1.0
-	if Input.is_action_just_pressed("move_left"):
-		dir.x -= 1.0
-	if Input.is_action_just_pressed("move_right"):
-		dir.x += 1.0
-	
-	return dir
-
-
-#func _on_controller_emerged():
-	#camera.environment = null
-#
-#
-#func _on_controller_subemerged():
-	#camera.environment = underwater_env
